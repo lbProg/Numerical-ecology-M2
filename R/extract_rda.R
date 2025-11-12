34c139ca7daced961012c4d8b1aefdcd2c584e53
Lucien Bastin <lucien.bastin@protonmail.com>
2025-11-12 17:52:14 +0100

    Add function to get RDA scores from vegan

---
+++ R/extract_rda.R
@@ -0,0 +1,63 @@
+extract_rda <- function(res_rda, scaling = 1, select_spe = NULL, site_sc = "lc") {
+  n_sp <- length(rda$colsum)
+  if (is.null(select_spe)) {
+    vec <- 1:n_sp
+  } else {
+    vec <- select_spe
+  }
+
+  Tot_var <- res_rda$tot_chi
+  eig_val <- c(res_rda$CCA$eig, res_rda$CA$eig)
+  Lambda <- diag(eig_val)
+  eig_val_rel <- eig_val / Tot_var
+  Diag <- diag(sqrt(eig_val_rel))
+  U_sc1 <- cbind(res_rda$CCA$v, res_rda$CA$v)
+  U_sc2 <- U_sc1 %*% sqrt(Lambda)
+  colnames(U_sc2) <- colnames(U_sc1)
+  n <- nrow(res_rda$CCA$u)
+  Z_sc2 <- cbind(res_rda$CCA$u, res_rda$CA$u) * sqrt(n - 1)
+  Z_sc1 <- Z_sc2 %*% sqrt(Lambda)
+  colnames(Z_sc1) <- colnames(Z_sc2)
+  F_sc2 <- cbind(res_rda$CCA$wa, res_rda$CA$u) * sqrt(n - 1)
+  F_sc1 <- F_sc2 %*% sqrt(Lambda)
+  colnames(F_sc1) <- colnames(F_sc2)
+  BP_sc2 <- res_rda$CCA$biplot
+  BP_sc2 <- cbind(BP_sc2, matrix(0, nrow = nrow(BP_sc2), ncol = length(eig_val) - k))
+  colnames(BP_sc2) <- colnames(F_sc2)
+  BP_sc1 <- BP_sc2 %*% Diag
+  colnames(BP_sc1) <- colnames(BP_sc2)
+
+  if (!is.null(res.rda$CCA$centroids)) {
+    centroids_sc2 <- res_rda$CCA$centroids * sqrt(n - 1) # Centroids, scaling=2
+    centroids_sc2 <- cbind(centroids_sc2, matrix(
+      0,
+      nrow = nrow(centroids_sc2),
+      ncol = length(eig_val) - k
+    ))
+    colnames(centroids_sc2) <- colnames(F.sc2)
+    centroids_sc1 <- centroids_sc2 %*% sqrt(Lambda)      # Centroids, scaling=1
+    colnames(centroids_sc1) <- colnames(centroids_sc2)
+  }
+  centroids_present <- TRUE
+  
+  if (scaling == 1) {
+    if (site_sc == "lc") {
+      sit_sc <- Z_sc1
+    } else {
+      sit_sc <- F_sc1
+    }
+    spe_sc <- U_sc1[vec, ]
+    BP_sc  <- BP_sc1
+    if (centroids_present)
+      centroids <- centroids_sc1
+  }
+
+  # Return coordinates of everything
+
+  list(
+    site_scores = site_sc,
+    species_scores = spe_sc,
+    env_sc = BP_sc,
+    centroids = centroids
+  )
+}
