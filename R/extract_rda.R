b916a81569aae6dbbfc433d8b2126bc7d198ffe7
Lucien Bastin <lucien.bastin@protonmail.com>
2025-11-12 18:29:42 +0100

    Add stretch factor for species and env scores

--- R/extract_rda.R
+++ R/extract_rda.R
@@ -1,12 +1,44 @@
-extract_rda <- function(res_rda, scaling = 1, select_spe = NULL, site_sc = "lc") {
-  n_sp <- length(rda$colsum)
+extract_rda <- function(
+  res_rda,
+  scaling = 1,
+  select_spe = NULL,
+  site_sc = "lc",
+  mult_spe = 1,
+  mult_arrow = 1,
+  optimum = TRUE,
+  plot_centr = TRUE,
+  label_centr = TRUE
+) {
+  'stretch' <- function(sites, mat, n) {
+    # Compute stretching factor for the species or environmental arrows
+    # First, compute the longest distance to centroid for the sites
+    tmp1 <- rbind(rep(0, ncol(sites)), sites)
+    D <- dist(tmp1)
+    target <- max(D[1:n])
+    # Then, compute the longest distance to centroid for the species or
+    # environmental arrows
+    if (is.matrix(mat)) {
+      p <- nrow(mat)
+      tmp2 <- rbind(rep(0, ncol(mat)), mat)
+      D <- dist(tmp2)
+      longest <- max(D[1:p])
+    } else {
+      tmp2 <- rbind(rep(0, ncol(mat)), mat)
+      longest <- dist(tmp2)
+    }
+    fact <- target / longest
+  }
+  
+  n_sp <- length(res_rda$colsum)
+  k <- length(res_rda$CCA$eig)
+
   if (is.null(select_spe)) {
     vec <- 1:n_sp
   } else {
     vec <- select_spe
   }
 
-  Tot_var <- res_rda$tot_chi
+  Tot_var <- res_rda$tot.chi
   eig_val <- c(res_rda$CCA$eig, res_rda$CA$eig)
   Lambda <- diag(eig_val)
   eig_val_rel <- eig_val / Tot_var
@@ -22,24 +54,30 @@ extract_rda <- function(res_rda, scaling = 1, select_spe = NULL, site_sc = "lc")
   F_sc1 <- F_sc2 %*% sqrt(Lambda)
   colnames(F_sc1) <- colnames(F_sc2)
   BP_sc2 <- res_rda$CCA$biplot
-  BP_sc2 <- cbind(BP_sc2, matrix(0, nrow = nrow(BP_sc2), ncol = length(eig_val) - k))
+  BP_sc2 <- cbind(
+    BP_sc2,
+    matrix(0, nrow = nrow(BP_sc2), ncol = length(eig_val) - k)
+  )
   colnames(BP_sc2) <- colnames(F_sc2)
   BP_sc1 <- BP_sc2 %*% Diag
   colnames(BP_sc1) <- colnames(BP_sc2)
 
-  if (!is.null(res.rda$CCA$centroids)) {
+  if (!is.null(res_rda$CCA$centroids)) {
     centroids_sc2 <- res_rda$CCA$centroids * sqrt(n - 1) # Centroids, scaling=2
-    centroids_sc2 <- cbind(centroids_sc2, matrix(
-      0,
-      nrow = nrow(centroids_sc2),
-      ncol = length(eig_val) - k
-    ))
-    colnames(centroids_sc2) <- colnames(F.sc2)
-    centroids_sc1 <- centroids_sc2 %*% sqrt(Lambda)      # Centroids, scaling=1
+    centroids_sc2 <- cbind(
+      centroids_sc2,
+      matrix(
+        0,
+        nrow = nrow(centroids_sc2),
+        ncol = length(eig_val) - k
+      )
+    )
+    colnames(centroids_sc2) <- colnames(F_sc2)
+    centroids_sc1 <- centroids_sc2 %*% sqrt(Lambda) # Centroids, scaling=1
     colnames(centroids_sc1) <- colnames(centroids_sc2)
   }
   centroids_present <- TRUE
-  
+
   if (scaling == 1) {
     if (site_sc == "lc") {
       sit_sc <- Z_sc1
@@ -47,15 +85,39 @@ extract_rda <- function(res_rda, scaling = 1, select_spe = NULL, site_sc = "lc")
       sit_sc <- F_sc1
     }
     spe_sc <- U_sc1[vec, ]
-    BP_sc  <- BP_sc1
-    if (centroids_present)
+    BP_sc <- BP_sc1
+    if (centroids_present) {
       centroids <- centroids_sc1
+    }
   }
+
+  fact_spe <- 1
+  fact_env <- 1
+  if (centroids_present & (plot_centr | label_centr)) {
+    to_plot <- which(!(rownames(BP_sc) %in% rownames(centroids)))
+  } else {
+    to_plot <- 1:nrow(BP_sc)
+  }
+
+  if (optimum) {
+    fact_spe <- stretch(sit_sc, spe_sc, n)
+    quant_env_present <- TRUE
+    fact_env <- stretch(
+      sit_sc,
+      BP_sc[to_plot, ],
+      n
+    )
+  }
+
+  print(fact_spe)
+
+  spe_sc <- spe_sc * fact_spe * mult_spe
+  BP_sc <- BP_sc * fact_env * mult_arrow
 
   # Return coordinates of everything
 
   list(
-    site_scores = site_sc,
+    site_scores = sit_sc,
     species_scores = spe_sc,
     env_sc = BP_sc,
     centroids = centroids
